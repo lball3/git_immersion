d81b5877072062b9af40bc955991b27456c7f071
Louis Ball <louis.ball@me.com>
2015-11-23 23:35:17 -0800

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,6 @@
-# Default is "World"
-# Author: Louis Ball (louis@somewhere.com)
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
 
 
